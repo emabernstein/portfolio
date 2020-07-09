4a5d4b2ead6d6161301ff3c2be730092c52eb584
Emma Bernstein <60675727+emabernstein@users.noreply.github.com>
2020-07-09 11:38:21 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+<img src="./public/assets/logo.png" width="120">
+
+# tailwind-template
+
+Little template repository for basic tailwind stuff
+
+## Running ⛹️‍♀️
+
+should be able to
+
+```sh
+yarn # or npm i
+yarn serve # or npm run serve
+```
+
+or just open `index.html` in your browser 😊
+
+## Building 👷‍♀️
+
+```sh
+yarn build # or npm run build
+```
+
+just builds tailwind to not be huge!
+
+## Example 🚀
+
+[base on netlify](https://xenodochial-pasteur-bea80c.netlify.app/)
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+const minMax = {
+  min: 'min-content',
+  max: 'max-content',
+}
+module.exports = {
+  purge: [
+    './public/**/*.html',
+    './public/**/*.vue',
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['Inter', 'system-ui', '-apple-system', 'BlinkMacSystemFont', "Segoe UI", 'Roboto', "Helvetica Neue", 'Arial', "Noto Sans", 'sans-serif', "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
+      serif: ['Georgia', 'Cambria', "Times New Roman", 'Times', 'serif'],
+      mono: ['Menlo', 'Monaco', 'Consolas', "Liberation Mono", "Courier New", 'monospace']
+    },
+    extend: {
+      width: minMax,
+      height: minMax,
+    },
+  },
+  variants: {},
+  plugins: [],
+}
